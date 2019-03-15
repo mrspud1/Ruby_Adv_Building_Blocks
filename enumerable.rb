b0ed788b552fad6524ef7e89849596cceb267254
Matt <mbrett19@outlook.com>
2019-03-15 22:07:44 +1300

    Updated enumerable, now includes all? and any?

--- enumerable.rb
+++ enumerable.rb
@@ -9,14 +9,35 @@ module Enumerable
             yield(self[x], x)
         end
     end
+    def my_select
+        arr = []
+        self.my_each {|x| arr.push(x) if yield(x)}
+        arr
+    end
+    def my_all?
+        self.my_each {|x| 
+            unless yield(x)
+                return false
+            end
+        }
+        return true
+    end
+    def my_any?
+        self.my_each {|x| 
+            if yield(x)
+                return true
+            end
+        }
+        return false
+    end
 end
+
+
 
 array1 = ["hi",2,3]
-
-array1.each_with_index do |item, index|
-   puts item.to_s + index.to_s
+array2 = array1.my_any? do |item|
+    item.is_a? String
 end
 
-array1.my_each_with_index do |item, index|
-    puts item.to_s + index.to_s
-end
+puts array2
+
